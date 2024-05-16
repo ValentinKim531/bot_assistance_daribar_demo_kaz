50e4b4ad5e4595df5fb19f1e3e0cb614e5224f5e
Valentin Kim <valentink@yandex.ru>
2024-05-16 12:30:37 +0500

    upgrade code

--- telegram_bot_v2kaz.py
+++ telegram_bot_v2kaz.py
@@ -6,44 +6,59 @@ from aiogram.filters import CommandStart
 from aiogram.dispatcher.router import Router
 from dotenv import load_dotenv
 import os
-from yandex_cloud_api_kaz import recognize_speech, synthesize_speech
-from openai_gpt import process_question
 import re
 import logging
 
+from openai_gpt import process_question
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 load_dotenv()
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-YANDEX_IAM_TOKEN = os.getenv("YANDEX_IAM_TOKEN")
 YANDEX_FOLDER_ID = os.getenv("YANDEX_FOLDER_ID")
+YANDEX_OAUTH_TOKEN = os.getenv("YANDEX_OAUTH_TOKEN")
+
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 router = Router()
 
+YANDEX_IAM_TOKEN = None
 
-def fetch_medicine_info():
+def get_iam_token():
+    global YANDEX_IAM_TOKEN
+    url = "https://iam.api.cloud.yandex.net/iam/v1/tokens"
+    payload = {"yandexPassportOauthToken": YANDEX_OAUTH_TOKEN}
+    response = requests.post(url, json=payload)
+    response.raise_for_status()
+    YANDEX_IAM_TOKEN = response.json()["iamToken"]
+    logger.info(f"Received new IAM token: {YANDEX_IAM_TOKEN}")
+
+async def refresh_iam_token():
+    while True:
+        await asyncio.sleep(6 * 3600)
+        get_iam_token()
+        logger.info("IAM token refreshed")
+
+def fetch_medicine_info(sku):
     """Отправка запроса к API для получения данных о лекарстве."""
     url = 'https://prod-backoffice.daribar.com/api/v1/products/search?city=%D0%90%D0%BB%D0%BC%D0%B0%D1%82%D1%8B'
     headers = {
         'accept': 'application/json, text/plain, */*',
         'accept-language': 'en-US,en;q=0.9,ru;q=0.8',
-        'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXN0b21lciI6ZmFsc2UsImV4cCI6MTcxNTcwODM0NiwibmFtZSI6IiIsInBob25lIjoiNzc3NzU4NDY5NjEiLCJyZWZyZXNoIjpmYWxzZSwicm9sZSI6ImFkbWluIiwic2Vzc2lvbl9pZCI6IjE0MGE4ZTg3LWQzZTctNGE4Yi1iODE1LTEyZjE2YjBiZGU0NiJ9.JI2N8d93qDcH5sTOOI0bAo3aRJcjK02ZsMjez_Xd3wQ',  # Убедитесь, что токен актуален
+        'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXN0b21lciI6ZmFsc2UsImV4cCI6MTcxNTcwODM0NiwibmFtZSI6IiIsInBob25lIjoiNzc3NzU4NDY5NjEiLCJyZWZyZXNoIjpmYWxzZSwicm9sZSI6ImFkbWluIiwic2Vzc2lvbl9pZCI6IjE0MGE4ZTg3LWQzZTctNGE4Yi1iODE1LTEyZjE2YjBiZGU0NiJ9.JI2N8d93qDcH5sTOOI0bAo3aRJcjK02ZsMjez_Xd3wQ',
         'content-type': 'application/json',
         'origin': 'https://daribar.kz',
         'referer': 'https://daribar.kz/'
     }
-    data = '[{"sku":"75c9de5e-0669-41b0-b07b-d6d545c6711a","count_desired":1}]'
+    data = f'[{{"sku":"{sku}","count_desired":1}}]'
     response = requests.post(url, headers=headers, data=data)
+    response.raise_for_status()
     return response.json()
-
 
 @router.message(CommandStart())
 async def process_start_command(message: types.Message):
-    await message.answer('Сәлеметсіз бе! Маған дәрі-дәрмектің аты жазылған дауыстық поштаны жіберіңіз.\n Сондай-ақ операцияның демонстрациялық бейнесін келесі сілтемеден көре аласыз:\nhttps://youtube.com/shorts/6pJ04x-M-XQ')
-
+    await message.answer('Сәлеметсіз бе! Маған бір дәрі-дәрмектің атауы бар дауыстық хабарлама жіберіңіз (демонстрацияға рұқсат етілген дәрілік нұсқалар: ибупрофен, анальгин, аспирин, қызыл май).\nСонымен қатар демонстрациялық бейнені мына сілтемеден көре аласыз:\nhttps://youtube .com/shorts /6pJ04x-M-XQ')
 
 @router.message(F.voice)
 async def handle_voice_message(message: types.Message):
@@ -65,49 +80,54 @@ async def handle_voice_message(message: types.Message):
         response_text = await process_question(speech_text)
         logger.info(f"Response text from GPT: {response_text}")
 
-        # Получение данных о лекарствах и форматирование ответа
-        if response_text.lower().strip() == 'кызылмай' or response_text.lower().strip() == 'кызыл май':
-            api_response = fetch_medicine_info()
-            formatted_response = format_response(api_response)
-            logger.info(f"Formatted response for synthesis: {formatted_response}")
+        if response_text.lower().strip() in ['кызылмай', 'кызыл май', 'аспирин', 'анальгин', 'ибупрофен']:
+            if response_text.lower().strip() == 'кызылмай' or response_text.lower().strip() == 'кызыл май':
+                api_response = fetch_medicine_info('75c9de5e-0669-41b0-b07b-d6d545c6711a')
+            elif response_text.lower().strip() == 'аспирин':
+                api_response = fetch_medicine_info('ba5e98ee-12e0-4dac-9730-d15f67d51942')
+            elif response_text.lower().strip() == 'анальгин':
+                api_response = fetch_medicine_info('100311')
+            elif response_text.lower().strip() == 'ибупрофен':
+                api_response = fetch_medicine_info('37025703-0b63-4c4a-bdf7-2a40cd8cc236')
+            else:
+                api_response = {}
 
-            # Перевод сформированного ответа на казахский язык
-            translated_text = translate_text(formatted_response, source_lang='ru', target_lang='kk')
-            logger.info(f"Translated text: {translated_text}")
+            if api_response:
+                formatted_response = format_response(api_response)
+                logger.info(f"Formatted response for synthesis: {formatted_response}")
 
-            # Синтез речи из переведенного текста
-            audio_response_bytes = synthesize_speech(translated_text)
+                translated_text = translate_text(formatted_response, source_lang='ru', target_lang='kk')
+                logger.info(f"Translated text: {translated_text}")
 
-            mp3_audio_path = "response.mp3"
-            with open(mp3_audio_path, "wb") as mp3_audio_file:
-                mp3_audio_file.write(audio_response_bytes)
+                audio_response_bytes = synthesize_speech(translated_text)
 
-            try:
-                await message.answer_voice(voice=types.FSInputFile(mp3_audio_path), caption=translated_text)
-                logger.info("Voice response sent successfully.")
-            except Exception as e:
-                logger.error(f"Failed to send voice response: {e}")
-                await message.answer("Failed to send the voice response.")
-            finally:
-                if os.path.exists(mp3_audio_path):
-                    os.remove(mp3_audio_path)
-                    logger.info(f"Deleted file {mp3_audio_path}.")
+                mp3_audio_path = "response.mp3"
+                with open(mp3_audio_path, "wb") as mp3_audio_file:
+                    mp3_audio_file.write(audio_response_bytes)
 
+                try:
+                    await message.answer_voice(voice=types.FSInputFile(mp3_audio_path), caption=translated_text)
+                    logger.info("Voice response sent successfully.")
+                except Exception as e:
+                    logger.error(f"Failed to send voice response: {e}")
+                    await message.answer("Failed to send the voice response.")
+                finally:
+                    if os.path.exists(mp3_audio_path):
+                        os.remove(mp3_audio_path)
+                        logger.info(f"Deleted file {mp3_audio_path}.")
+            else:
+                await message.answer(text='К сожалению, данное лекарство не найдено.')
         else:
             await message.answer(
-                text='Демонстрацияның бөлігі ретінде «қызыл май» сөзі бар сөйлемді айтыңыз.\n Сондай-ақ операцияның демонстрациялық бейнесін келесі сілтемеден көре аласыз:\nhttps://youtube.com/shorts/6pJ04x-M-XQ')
+                text='Демонстрацияның бір бөлігі ретінде келесі қолайлы дәрі атауларының бірін қамтитын сөйлемді айтыңыз: ибупрофен, аспирин, анальгин, қызыл май.\nСонымен қатар демонстрациялық бейнені мына сілтемеден көре аласыз:\nhttps://youtube.com/shorts /6pJ04x-M- XQ')
 
     except Exception as e:
         logger.error(f"Failed to send voice: {e}")
         await message.answer(
-            text='Техникалық себептерге байланысты сұраныс өңделмеді.\nДегенмен, операцияның демонстрациялық бейнесін келесі сілтемеден көре аласыз:\nhttps://youtube.com/shorts/6pJ04x-M-XQ')
-
-
-
+            text='Техникалық себептерге байланысты сұрау өңделмеді.\nАлайда демонстрациялық бейнені мына сілтемеден көре аласыз:\nhttps://youtube.com/shorts/6pJ04x-M-XQ')
 
 
 def format_response(data):
-    """Форматирование данных API в строку ответа."""
     results = []
     for item in data['result'][:3]:
         pharmacy_info = f"Препарат {item['products'][0]['name']}\n Адрес:\n город {item['source']['city']},\n {item['source']['address']},\n стоимость препарата {item['products'][0]['base_price']} тенге\n"
@@ -149,10 +169,62 @@ def translate_text(text, source_lang='ru', target_lang='kk'):
         return "Перевод не удался."
 
 
+def recognize_speech(audio_content):
+    global YANDEX_IAM_TOKEN
+    url = f"https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?folderId={YANDEX_FOLDER_ID}&lang=ru-RU"
+    headers = {"Authorization": f"Bearer {YANDEX_IAM_TOKEN}"}
+
+    logger.info(f"Sending {len(audio_content)} bytes to Yandex STT.")
+    response = requests.post(url, headers=headers, data=audio_content)
+    response.raise_for_status()
+
+    if response.status_code == 200:
+        result = response.json().get("result")
+        logger.info(f"Recognition result: {result}")
+        return result
+    else:
+        error_message = f"Failed to recognize speech, status code: {response.status_code}"
+        logger.error(error_message)
+        raise Exception(error_message)
+
+
+def synthesize_speech(text):
+    global YANDEX_IAM_TOKEN
+    url = 'https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize'
+    headers = {'Authorization': f'Bearer {YANDEX_IAM_TOKEN}'}
+    data = {
+        'text': text,
+        'lang': 'kk-KK',
+        'voice': 'amira',
+        'folderId': YANDEX_FOLDER_ID,
+        'format': 'mp3',
+        'sampleRateHertz': 48000,
+    }
+
+    response = requests.post(url, headers=headers, data=data, stream=True)
+    logger.info(f"Status Code: {response.status_code}")
+    logger.info(f"Response Headers: {response.headers}")
+
+    if response.status_code == 200:
+        audio_content = response.content
+        logger.info(f"Received audio content length: {len(audio_content)} bytes")
+        with open("response.mp3", "wb") as file:
+            file.write(audio_content)
+        logger.info("Audio content saved as 'response.mp3'.")
+        return audio_content
+    else:
+        error_message = f"Failed to synthesize speech, status code: {response.status_code}, response text: {response.text}"
+        logger.error(error_message)
+        raise Exception(error_message)
+
+
 dp.include_router(router)
 
 
 async def main():
+    get_iam_token()
+    task = asyncio.create_task(refresh_iam_token())  # Запускаем задачу обновления токена
+    _ = task
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, skip_updates=True)
 
--- yandex_cloud_api_kaz.py
+++ yandex_cloud_api_kaz.py
@@ -3,17 +3,19 @@ import os
 from dotenv import load_dotenv
 import logging
 
+from telegram_bot_v2kaz import get_iam_token
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
 load_dotenv()
 YANDEX_FOLDER_ID = os.getenv("YANDEX_FOLDER_ID")
-YANDEX_IAM_TOKEN = os.getenv("YANDEX_IAM_TOKEN")
-
+YANDEX_OAUTH_TOKEN = os.getenv("YANDEX_OAUTH_TOKEN")
+YANDEX_IAM_TOKEN = None
 
 def recognize_speech(audio_content):
+    if not YANDEX_IAM_TOKEN:
+        get_iam_token()
     url = f"https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?folderId={YANDEX_FOLDER_ID}&lang=ru-RU"
     headers = {"Authorization": f"Bearer {YANDEX_IAM_TOKEN}"}
 
@@ -31,6 +33,8 @@ def recognize_speech(audio_content):
 
 
 def synthesize_speech(text):
+    if not YANDEX_IAM_TOKEN:
+        get_iam_token()
     url = 'https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize'
     headers = {'Authorization': f'Bearer {YANDEX_IAM_TOKEN}'}
     data = {
